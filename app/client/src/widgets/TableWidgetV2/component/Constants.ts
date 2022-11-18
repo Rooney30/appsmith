e8f93b27658c865389d6446baa4dcaebc8d04fc8
balajisoundar <balaji@appsmith.com>
2022-11-18 10:56:38 +0530

    fix type error

--- app/client/src/widgets/TableWidgetV2/component/Constants.ts
+++ app/client/src/widgets/TableWidgetV2/component/Constants.ts
@@ -161,7 +161,7 @@ export interface SelectCellProperties {
   serverSideFiltering?: boolean;
   placeholderText?: string;
   resetFilterTextOnClose?: boolean;
-  selectOptions?: DropdownOption[] | DropdownOption[][];
+  selectOptions?: DropdownOption[];
 }
 
 export interface ImageCellProperties {
